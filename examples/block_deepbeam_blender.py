1ec54a0a9ad62f7249fc5c74439fccc20b879558
andrewtliew <liew@arch.ethz.ch>
2019-02-04 16:34:36 +0100

    - Blender .mesh_extrude() and .plot_voxels() updated. - Example block_deepbeam_blender updated.

---
+++ examples/block_deepbeam_blender.py
@@ -0,0 +1,81 @@
+
+from compas_fea.cad import blender
+from compas_fea.structure import ElasticIsotropic
+from compas_fea.structure import ElementProperties as Properties
+from compas_fea.structure import GeneralStep
+from compas_fea.structure import PinnedDisplacement
+from compas_fea.structure import PointLoad
+from compas_fea.structure import SolidSection
+from compas_fea.structure import Structure
+
+from compas_blender.utilities import get_object_by_name
+
+
+__author__    = ['Andrew Liew <liew@arch.ethz.ch>']
+__copyright__ = 'Copyright 2018, BLOCK Research Group - ETH Zurich'
+__license__   = 'MIT License'
+__email__     = 'liew@arch.ethz.ch'
+
+
+# Structure
+
+mdl = Structure(name='block_deepbeam', path='C:/Temp/')
+
+# Mesh
+
+ds = 0.05
+Lx = 1
+Ly = 2
+Lz = 1
+
+mesh = get_object_by_name(name='plane')
+blender.mesh_extrude(mdl, mesh=mesh, layers=int(Lz / ds), thickness=ds, blocks_name='elset_blocks')
+
+# Sets
+
+pins = [[ds, ds, 0], [Lx - ds, ds, 0], [Lx - ds, Ly - ds, 0], [ds, Ly - ds, 0]]
+mdl.add_set(name='nset_supports', type='node', selection=[mdl.check_node_exists(i) for i in pins])
+mdl.add_set(name='nset_load', type='node', selection=[mdl.check_node_exists([Lx * 0.5, Ly * 0.5, Lz])])
+
+# Materials
+
+mdl.add(ElasticIsotropic(name='mat_elastic', E=10**10, v=0.3, p=1))
+
+# Sections
+
+mdl.add(SolidSection(name='sec_solid'))
+
+# Properties
+
+mdl.add(Properties(name='ep_solid', material='mat_elastic', section='sec_solid', elset='elset_blocks'))
+
+# Displacements
+
+mdl.add(PinnedDisplacement(name='disp_pinned', nodes='nset_supports'))
+
+# Loads
+
+mdl.add(PointLoad(name='load_point', nodes='nset_load', z=-1))
+
+# Steps
+
+mdl.add([
+    GeneralStep(name='step_bc', displacements=['disp_pinned']),
+    GeneralStep(name='step_load', loads=['load_point']),
+])
+mdl.steps_order = ['step_bc', 'step_load']
+
+# Summary
+
+mdl.summary()
+
+# Run
+
+mdl.analyse_and_extract(software='abaqus', fields=['u', 's'], components=['ux', 'uy', 'uz', 'smises'])
+
+mdl.save_to_obj()
+
+# Plot
+
+# blender.plot_data(mdl, step='step_load', field='smises', cbar=[0, 2])
+# blender.plot_voxels(mdl, step='step_load', field='smises', vdx=ds, cbar=[0, 1.5])
--- src/compas_fea/cad/blender.py
+++ src/compas_fea/cad/blender.py
@@ -4,7 +4,6 @@ from __future__ import division
 from __future__ import print_function
 
 from compas_blender.geometry import BlenderMesh
-# from compas_blender.helpers import mesh_from_bmesh
 from compas_blender.utilities import clear_layer
 from compas_blender.utilities import draw_cylinder
 from compas_blender.utilities import draw_plane
@@ -12,6 +11,7 @@ from compas_blender.utilities import draw_line
 from compas_blender.utilities import get_meshes
 from compas_blender.utilities import get_objects
 from compas_blender.utilities import get_points
+from compas_blender.utilities import mesh_from_bmesh
 from compas_blender.utilities import set_select
 from compas_blender.utilities import set_deselect
 # from compas_blender.utilities import get_object_location
@@ -25,12 +25,12 @@ from compas.geometry import cross_vectors
 from compas.geometry import subtract_vectors
 
 from compas_fea.utilities import colorbar
-# from compas_fea.utilities import extrude_mesh
+from compas_fea.utilities import extrude_mesh
 # from compas_fea.utilities import network_order
 from compas_fea.utilities import discretise_faces
 from compas_fea.utilities import postprocess
 # from compas_fea.utilities import tets_from_vertices_faces
-# from compas_fea.utilities import plotvoxels
+from compas_fea.utilities import plotvoxels
 
 from numpy import array
 from numpy import hstack
@@ -62,8 +62,8 @@ __all__ = [
 #     'add_elset_from_bmeshes',
 #     'add_nset_from_objects',
     'plot_data',
-#     'plot_voxels',
-#     'mesh_extrude',
+    'plot_voxels',
+    'mesh_extrude',
     'plot_reaction_forces',
     'plot_concentrated_forces',
 ]
@@ -434,40 +434,53 @@ def add_nsets_from_layers(structure, layers):
         structure.add_set(name=layer, type='node', selection=nodes)
 
 
-# def mesh_extrude(structure, bmesh, layers, thickness, mesh_name='', links_name='', blocks_name=''):
+def mesh_extrude(structure, mesh, layers, thickness, mesh_name='', links_name='', blocks_name='', points_name='',
+                 plot_mesh=False, plot_links=False, plot_blocks=False, plot_points=False):
 
-#     """ Extrudes a Blender mesh and adds/creates elements.
+    """ Extrudes a Blender mesh and adds/creates elements.
 
-#     Parameters
-#     ----------
-#     structure : obj
-#         Structure object to update.
-#     bmesh : obj
-#         Blender mesh object.
-#     layers : int
-#         Number of layers.
-#     thickness : float
-#         Layer thickness.
-#     blocks_name : str
-#         Name of set for solid elements.
-#     mesh_name : str
-#         Name of set for mesh on final surface.
-#     links_name : str
-#         Name of set for adding links along extrusion.
+    Parameters
+    ----------
+    structure : obj
+        Structure object to update.
+    mesh : obj
+        Blender mesh object.
+    layers : int
+        Number of layers.
+    thickness : float
+        Layer thickness.
+    mesh_name : str
+        Name of set for mesh on final surface.
+    links_name : str
+        Name of set for adding links along extrusion.
+    blocks_name : str
+        Name of set for solid elements.
+    points_name : str
+        Name of aded points.
+    plot_mesh : bool
+        Plot outer mesh.
+    plot_links : bool
+        Plot links.
+    plot_blocks : bool
+        Plot blocks.
+    plot_points : bool
+        Plot end points.
 
-#     Returns
-#     -------
-#     None
+    Returns
+    -------
+    None
 
-#     Notes
-#     -----
-#     - Extrusion is along the vertex normals.
+    Notes
+    -----
+    - Extrusion is along the vertex normals.
 
-#     """
+    """
 
-#     mesh = mesh_from_bmesh(bmesh)
-#     extrude_mesh(structure=structure, mesh=mesh, layers=layers, thickness=thickness, mesh_name=mesh_name,
-#                  links_name=links_name, blocks_name=blocks_name)
+    mesh = mesh_from_bmesh(mesh)
+    extrude_mesh(structure=structure, mesh=mesh, layers=layers, thickness=thickness, mesh_name=mesh_name,
+                 links_name=links_name, blocks_name=blocks_name)
+
+    # ADD PLOTTING FUNCTIONS HERE
 
 
 def plot_data(structure, step, field='um', layer=None, scale=1.0, radius=0.05, cbar=[None, None], iptype='mean',
@@ -559,9 +572,10 @@ def plot_data(structure, step, field='um', layer=None, scale=1.0, radius=0.05, c
     # Plot meshes
 
     npts = 8
-    mesh_faces = []
-    pipes      = []
-    mesh_add   = []
+    mesh_faces  = []
+    block_faces = [[0, 1, 2, 3], [4, 5, 6, 7], [0, 1, 5, 4], [1, 2, 6, 5], [2, 3, 7, 6], [3, 0, 4, 7]]
+    pipes       = []
+    mesh_add    = []
 
     for element, nodes in enumerate(elements):
 
@@ -582,14 +596,19 @@ def plot_data(structure, step, field='um', layer=None, scale=1.0, radius=0.05, c
 
             try:
                 blendermesh = BlenderMesh(object=pipe)
-                blendermesh.set_vertices_colors({i: col1 for i in range(0, 2*npts, 2)})
-                blendermesh.set_vertices_colors({i: col2 for i in range(1, 2*npts, 2)})
+                blendermesh.set_vertices_colors({i: col1 for i in range(0, 2 * npts, 2)})
+                blendermesh.set_vertices_colors({i: col2 for i in range(1, 2 * npts, 2)})
             except:
                 pass
 
         elif n in [3, 4]:
 
             mesh_faces.append(nodes)
+
+        elif n == 8:
+
+            for block in block_faces:
+                mesh_faces.append([nodes[i] for i in block])
 
     if mesh_faces:
 
@@ -731,67 +750,72 @@ def plot_concentrated_forces(structure, step, layer=None, scale=1.0):
         set_object_property(object=line, property='cfm', value=cfm[i])
 
 
-# def plot_voxels(structure, step, field='smises', cbar=[None, None], iptype='mean', nodal='mean',
-#                 vdx=None, mode='', plot='vtk'):
+def plot_voxels(structure, step, field='smises', cbar=[None, None], iptype='mean', nodal='mean', vdx=None, mode=''):
 
-#     """ Voxel 4D visualisation.
+    """ Voxel 4D visualisation.
 
-#     Parameters
-#     ----------
-#     structure : obj
-#         Structure object.
-#     step : str
-#         Name of the Step.
-#     field : str
-#         Field to plot, e.g. 'smises'.
-#     cbar : list
-#         Minimum and maximum limits on the colorbar.
-#     iptype : str
-#         'mean', 'max' or 'min' of an element's integration point data.
-#     nodal : str
-#         'mean', 'max' or 'min' for nodal values.
-#     vdx : float
-#         Voxel spacing.
-#     mode : int
-#         mode or frequency number to plot, in case of modal, harmonic or buckling analysis.
-#     plot : str
-#         Plot voxels with 'vtk'.
+    Parameters
+    ----------
+    structure : obj
+        Structure object.
+    step : str
+        Name of the Step.
+    field : str
+        Field to plot, e.g. 'smises'.
+    cbar : list
+        Minimum and maximum limits on the colorbar.
+    iptype : str
+        'mean', 'max' or 'min' of an element's integration point data.
+    nodal : str
+        'mean', 'max' or 'min' for nodal values.
+    vdx : float
+        Voxel spacing.
+    mode : int
+        mode or frequency number to plot, in case of modal, harmonic or buckling analysis.
 
-#     Returns
-#     -------
-#     None
+    Returns
+    -------
+    None
 
-#     """
+    """
 
-#     # Node and element data
+    # Node and element data
 
-#     nodes = structure.nodes_xyz()
-#     elements = [structure.elements[i].nodes for i in sorted(structure.elements, key=int)]
-#     nodal_data = structure.results[step]['nodal']
-#     nkeys = sorted(structure.nodes, key=int)
-#     ux = [nodal_data['ux{0}'.format(mode)][i] for i in nkeys]
-#     uy = [nodal_data['uy{0}'.format(mode)][i] for i in nkeys]
-#     uz = [nodal_data['uz{0}'.format(mode)][i] for i in nkeys]
 
-#     try:
-#         data = [nodal_data['{0}{1}'.format(field, mode)][i] for i in nkeys]
-#         dtype = 'nodal'
-#     except(Exception):
-#         data = structure.results[step]['element'][field]
-#         dtype = 'element'
+    xyz        = structure.nodes_xyz()
+    elements   = [structure.elements[i].nodes for i in sorted(structure.elements, key=int)]
+    nodal_data = structure.results[step]['nodal']
+    nkeys      = sorted(structure.nodes, key=int)
 
-#     # Postprocess
+    ux = [nodal_data['ux{0}'.format(mode)][i] for i in nkeys]
+    uy = [nodal_data['uy{0}'.format(mode)][i] for i in nkeys]
+    uz = [nodal_data['uz{0}'.format(mode)][i] for i in nkeys]
 
-#     result = postprocess(nodes, elements, ux, uy, uz, data, dtype, 1, cbar, 1, iptype, nodal)
+    try:
+        data = [nodal_data[field + str(mode)][key] for key in nkeys]
+        dtype = 'nodal'
 
-#     try:
-#         toc, U, cnodes, fabs, fscaled, celements, eabs = result
-#         U = array(U)
-#         print('\n***** Data processed : {0:.3f} s *****'.format(toc))
-#     except:
-#         print('\n***** Error encountered during data processing or plotting *****')
+    except(Exception):
+        data = structure.results[step]['element'][field]
+        dtype = 'element'
 
-#     plotvoxels(values=fscaled, U=U, vdx=vdx, plot=plot)
+    # Postprocess
+
+    result = postprocess(xyz, elements, ux, uy, uz, data, dtype, 1, cbar, 1, iptype, nodal)
+
+    try:
+        toc, U, cnodes, fabs, fscaled, celements, eabs = result
+        U = array(U)
+        print('\n***** Data processed : {0:.3f} s *****'.format(toc))
+
+    except:
+        print('\n***** Error post-processing *****')
+
+    try:
+        plotvoxels(values=fscaled, U=U, vdx=vdx)
+
+    except:
+        print('\n***** Error plotting voxels *****')
 
 
 # ==============================================================================
@@ -800,4 +824,8 @@ def plot_concentrated_forces(structure, step, layer=None, scale=1.0):
 
 if __name__ == "__main__":
 
-    pass
+    from compas_fea.structure import Structure
+
+    mdl = Structure.load_from_obj(filename='C:/Temp/block_deepbeam.obj')
+
+    plot_voxels(mdl, step='step_load', field='smises', cbar=[0, 1.5], vdx=0.05, mode='')
