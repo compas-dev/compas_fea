1eaa894e58a60be2b9ee4607329426542beaf562
brgcode <vanmelet@ethz.ch>
2021-11-19 13:42:37 +0100

    Bump version to 0.3.3

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ optional_requirements = {}
 
 setup(
     name='compas_fea',
-    version='0.3.2',
+    version='0.3.3',
     description='compas finite element package',
     long_description=long_description,
     long_description_content_type='text/markdown',
